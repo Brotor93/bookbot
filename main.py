3da5ee7209bd6892084192bd3d6f0c751c08f17d
Brotor93 <marshall.udolf@icloud.com>
2025-03-31 12:40:18 -0600

    Changed count_words to get_num_words

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     filepath = "books/frankenstein.txt"  # Adjust the path if needed
     book_text = get_book_text(filepath)
     if "Error:" not in book_text:
-        num_words = count_words(book_text)
+        num_words = get_num_words(book_text)
         print(f"{num_words} words found in the document")
     else:
         print(book_text)
